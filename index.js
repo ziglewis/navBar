4b3f4663df0992a59a199b324c9583c38942170d
ziglewis <ziglewis48@gmail.com>
2023-05-02 01:15:57 +0100

    I added Js for the menu bar

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let popup = document.getElementById("menu-bar")
+
+function sidebar()  {
+    
+    popup.style.display = "block"
+}  
+
+
+function CloseSidebar()  {
+    popup.style.display = "none"
+
+}  
+
+
+
